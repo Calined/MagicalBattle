1b865dfed3bd0560171a15c6c54f2918b54ab419
Chris Manthey <calined@gmx.de>
2018-06-02 17:41:12 +0200

    background now displays

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 
+
+function Game() {
+
+    this.fps = 60;
+    this.timeStamp = 0;
+    this.time = 0;
+    this.dt = 0;
+
+}
+
+var game;
 var gameCanvas;
 var ctx;
 
@@ -9,8 +20,50 @@ window.onload = function () {
     gameCanvas = document.getElementById("gameCanvas");
     ctx = gameCanvas.getContext("2d");
 
-    console.log(window.innerWidth);
-
     gameCanvas.width = 160 * canvasScale;
     gameCanvas.height = 90 * canvasScale;
+
+    startGame();
+
 };
+
+
+function startGame() {
+
+    game = new Game();
+
+
+    requestAnimationFrame(updateCanvas);
+
+
+}
+
+
+
+function drawBackground() {
+    background = new Image();
+    background.src = "background.png";
+
+    ctx.drawImage(background, 0, 0);
+}
+
+
+
+function updateCanvas() {
+    setTimeout(function () {
+        requestAnimationFrame(updateCanvas);
+        var now = new Date().getTime(),
+            dt = now - (game.time || now);
+        game.dt = dt;
+
+        game.time = now;
+
+        game.timeStamp += game.dt;
+
+        drawBackground();
+
+
+    }, 1000 / game.fps);
+
+
+} 
